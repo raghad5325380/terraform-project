09ece5adfd8e3b01cc7d0d3dd5b71781acbf2e6a
Raghad <Raghad-Alshammari@outlook.sa>
2025-04-29 14:26:00 +0300

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -8,7 +8,7 @@ resource "azurerm_linux_virtual_machine" "chatbot-tf" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_chatbot_vm.pub") 
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+ON3RVvpW+ROWUbSMhte/ZGB0bC1oBvooP3LL5nG5Q9FNKpkcI7BjYNGKD1Daz0CLo54iSoZ1tYbykRlFBE+XJOnKIPlOO5rI31aBxE7L05W0BFCvp+v7OWXKLitRHramljiE67cp3ojvU/wQ3klXh1Odr86G1Kc3/PSivcBFhDbaO+IS/poGiL17zdMlZm4vJJKv5jcrkedcdwJBFbgNWrVz+cU7ZxobuqVBjFLv/ZoSNrwyduJW4icg4H62gE3p4ujTClLgMJUzC+rxTbdL5eIwzYgX6Exv3x8x/Uo4CkNMzfXyHPJHKBmqHd23uuosp/fOYgoIUIJE7vZrGzKFCKGzp7tASGPYdnvO0hNzgnh3yKR615X2UX8gADFJite1WOoXznsyfmLFw0xpLaqBJau3ewNrSIkTbZgDkFYVgDj8pB68IHFUOJtI3mvAdorh5bNnc9AdrFFHK2xnSfm3+S21HKVif/c+jKrMENXIUeQkPM9sPk18sGQ67WG/V161yV4ps1dcdOPn5rrtn2vQdYKAr6ZHigFzOvNVBcMkGkX/IxQ9GdNEHH0WfjxnDWI5yShhafzNi/4xdf+s6IrvGmNlC5Bvx6O7cxAurSvy9N8GBx4zxW4qw/wcmEKLG8Qqx23cDYpOimBQnYD2XsMsw4pGQXZGgllz/txU+NA2iw== raghadmohammed@rghds-MacBook-Pro.local" 
   }
 
   os_disk {
